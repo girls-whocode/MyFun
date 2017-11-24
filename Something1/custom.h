bd8e79ab52c9e4ced05099c2a3d59d4106fb97dc
jessica <jessica@jbrowns.com>
2017-11-24 09:13:54 -0500

    Playing with Netbeans and their Git Crap...

---
+++ Something1/custom.h
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   custom.h
+ * Author: jessica
+ *
+ * Created on November 24, 2017, 8:27 AM
+ */
+
+#ifndef CUSTOM_H
+#define CUSTOM_H
+
+int add (int, int);
+int subtract (int, int);
+int multiply (int, int);
+int divide (int, int);
+int screen ();
+
+#endif /* CUSTOM_H */
+
